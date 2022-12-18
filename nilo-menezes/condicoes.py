41ec2c344b8060169d9af369e0d7ed9c9da69132
Luiz Eduardo F. Menezes <luizefm@al.insper.edu.br>
2022-12-18 12:44:40 -0300

    Iniciado o capítulo 4 do livro de programação do Nilo Menezes.

---
+++ nilo-menezes/condicoes.py
@@ -0,0 +1,7 @@
+# Listagem 4.3 - Carro velho ou novo, depende da idade.
+
+idade = int(input("Digite a idade do seu carro: "))
+if idade <= 3:
+    print("Seu carro é novo")
+if idade > 3:
+    print("Seu carro é velho")
